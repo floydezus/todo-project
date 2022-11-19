96087b490f49ec320feca3dd03f05155b61ee473
aleksandr.tokarev <oulfc93@gmail.com>
2022-11-19 12:31:59 +0100

    --init tech

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.less';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        Testik
+      </header>
+    </div>
+  );
+}
+
+export default App;
